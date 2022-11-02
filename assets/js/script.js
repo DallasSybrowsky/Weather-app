7c014cafa0a6d8c273690fb95fb0ac90b4b8617a
[Dallas Sybrowsky] <dallas.sybrowsky@gmail.com>
2022-11-02 14:59:26 -0700

    Updated code with functional API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,20 @@
 // Variables
-// const searchBtn = document.querySelector('.btn');
-// const searchEl = document.querySelector('cityName');
+const searchBtn = document.querySelector('.btn');
+const searchEl = document.querySelector('#City');
 var APIKey = "6bd6fa04098d210723846067fd673874";
-// var city;
-// var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 // API call
-// const handleSearch = (event) =>{
-//     event.preventDefault();
-//     city = searchEl.value.trim();
-//     fetch(queryURL)
-//     .then(function(response){
-//         return response.json()
-//     })
-//     then(function(data){
-//         console.log(data)
-//     });
-// };
-
-// searchBtn.addEventListener('click', handleSearch)
-// city = document.getElementById(".cityName");
-
-const searchButton = document.querySelector('.btn');
-const searchEl = document.querySelector('.city');
-console.log(searchEl);
-const handleSearch = (event) => {
+const weatherSearch = (event) => {
     event.preventDefault();
-    // console.log(searchEl.trim());
-    let searchInput = searchEl;
-    let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${APIKey}`;
-    fetch(weatherURL)
+    let searchInput = searchEl.value.trim();
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${APIKey}`;
+    fetch(queryURL)
         .then(function(response){
             return response.json()
         })
         .then(function(data){
             console.log(data)
+            // console.log(data.document)
         });
 }
-searchButton.addEventListener('click', handleSearch)
+searchBtn.addEventListener('click', weatherSearch)
